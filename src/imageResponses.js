befb7270f3a911259345d3631eef1ee7b2643462
jbn0619 <jbn5379@g.rit.edu>
2021-08-31 13:45:29 -0400

    Finished assignment!

---
+++ src/imageResponses.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const img = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getImage = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(img);
+  response.end();
+};
+
+module.exports.img = img;
+module.exports.getImage = getImage;
